eac981e3005c84dd249e726fdbced432f554680f
Sahil Chaudhry <schaudhry@deloitte.com>
2018-06-29 14:21:14 -0600

    closing braces

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.get('/api/selectAccount', function(req,res) {
             else {
                 done();
                 result.dbUrl = process.env.DATABASE_URL;
-                res = ({type: 'Account', rows: result.rows);
+                res = ({type: 'Account', rows: result.rows});
             }
         }
     );
@@ -42,7 +42,7 @@ app.get('/api/selectContact', function(req,res) {
             else {
                 done();
                 result.dbUrl = process.env.DATABASE_URL;
-                res = ({type: 'Contact', rows: result.rows);
+                res = ({type: 'Contact', rows: result.rows});
             }
         }
     );
@@ -63,7 +63,7 @@ app.get('/api/selectOpportunity', function(req,res) {
             else {
                 done();
                 result.dbUrl = process.env.DATABASE_URL;
-                res = ({type: 'Opportunity', rows: result.rows);
+                res = ({type: 'Opportunity', rows: result.rows});
             }
         }
     );

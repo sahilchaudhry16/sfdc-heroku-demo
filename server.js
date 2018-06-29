3e70c2b29914bdbba270782912b7712d11623950
Sahil Chaudhry <schaudhry@deloitte.com>
2018-06-28 23:02:32 -0600

    updating endpoints

--- server.js
+++ server.js
@@ -45,13 +45,13 @@ app.get('/hello', function (req,res) {
     res.send('Hello');
  });
 
-app.get('/api/select', function(req,res) {
+app.get('/api/selectAccount', function(req,res) {
     console.log('Db url', process.env.DATABASE_URL);
     pg.connect('postgres://cdfbwviosuncnf:6c3b6526ed134fe49c0679ea892b796c3b30a707e9bf28f7d90679020efe82fe@ec2-54-227-247-225.compute-1.amazonaws.com:5432/d4di08hvdg5gto', function (err, conn, done) {
     if (err) res.status(400).json({error: 'Error in connecting'});
     if(!conn) res.status(400).json({error: 'Connection failed'});
     conn.query(
-        'SELECT * FROM salesforce.Account', function(err,result) {
+        'SELECT * FROM salesforce.Account LIMIT 5', function(err,result) {
             if (err) {
                 res.status(400).json({error: err.message});
             }
@@ -65,6 +65,51 @@ app.get('/api/select', function(req,res) {
 
     });
 });
+
+
+app.get('/api/selectContact', function(req,res) {
+    console.log('Db url', process.env.DATABASE_URL);
+    pg.connect('postgres://cdfbwviosuncnf:6c3b6526ed134fe49c0679ea892b796c3b30a707e9bf28f7d90679020efe82fe@ec2-54-227-247-225.compute-1.amazonaws.com:5432/d4di08hvdg5gto', function (err, conn, done) {
+    if (err) res.status(400).json({error: 'Error in connecting'});
+    if(!conn) res.status(400).json({error: 'Connection failed'});
+    conn.query(
+        'SELECT * FROM salesforce.Contact LIMIT 5', function(err,result) {
+            if (err) {
+                res.status(400).json({error: err.message});
+            }
+            else {
+                done();
+                result.dbUrl = process.env.DATABASE_URL;
+                res.json(result);
+            }
+        }
+    );
+
+    });
+});
+
+
+app.get('/api/selectOpportunity', function(req,res) {
+    console.log('Db url', process.env.DATABASE_URL);
+    pg.connect('postgres://cdfbwviosuncnf:6c3b6526ed134fe49c0679ea892b796c3b30a707e9bf28f7d90679020efe82fe@ec2-54-227-247-225.compute-1.amazonaws.com:5432/d4di08hvdg5gto', function (err, conn, done) {
+    if (err) res.status(400).json({error: 'Error in connecting'});
+    if(!conn) res.status(400).json({error: 'Connection failed'});
+    conn.query(
+        'SELECT * FROM salesforce.Opportunity LIMIT 5', function(err,result) {
+            if (err) {
+                res.status(400).json({error: err.message});
+            }
+            else {
+                done();
+                result.dbUrl = process.env.DATABASE_URL;
+                res.json(result);
+            }
+        }
+    );
+
+    });
+});
+
 
 app.listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));

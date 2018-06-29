e8fd01ae298b3001ad6f1d9be3f268850791de04
Sahil Chaudhry <schaudhry@deloitte.com>
2018-06-29 12:07:42 -0600

    updated result

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.get('/api/selectAccount', function(req,res) {
             else {
                 done();
                 result.dbUrl = process.env.DATABASE_URL;
-                res.json(result);
+                res.json(result.rows);
             }
         }
     );
@@ -42,7 +42,7 @@ app.get('/api/selectContact', function(req,res) {
             else {
                 done();
                 result.dbUrl = process.env.DATABASE_URL;
-                res.json(result);
+                res.json(result.rows);
             }
         }
     );
@@ -63,7 +63,7 @@ app.get('/api/selectOpportunity', function(req,res) {
             else {
                 done();
                 result.dbUrl = process.env.DATABASE_URL;
-                res.json(result);
+                res.json(result.rows);
             }
         }
     );
